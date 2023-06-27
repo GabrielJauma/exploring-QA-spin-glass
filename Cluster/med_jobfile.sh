2b9c52d110bfa963a689796cb678be9378bdbae8
gabriel <gabriel.jauma@gmail.com>
2023-06-27 15:38:56 +0200

    Update from work

--- Cluster/med_jobfile.sh
+++ Cluster/med_jobfile.sh
@@ -5,7 +5,7 @@
 # #SBATCH --tasks-per-node=5
 #SBATCH --mem=10GB
 module load miniconda3/4.11.0
-conda activate Architecture_v1
+conda activate name_of_your_environment
 if [ "$mode" = "binned" ]; then
   mpirun -n $SLURM_NTASKS python3.8 spin_glass_analysis_cluster_binned.py $adj $dist $n $T0 $Tf $MCS_avg $max_MCS $N_config $SLURM_JOBID $add
 elif [ "$mode" = "fast" ]; then
